3bb2c443d1ef5ec5b2c48b443fbb95251770a7ec
Hombobow <122179708+Hombobow@users.noreply.github.com>
2024-01-17 09:36:10 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,15 +1,38 @@
 
 
 public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+	public final static String e = "27182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
-		System.out.println("Replace this string with your answer!");
+	  double firstTenPrime = 0;
+	  
+	  for (int i = 0; i < newE.length() - 10; i++)
+	  {
+	    double num = Double.parseDouble(newE.substring(i, i+10));
+	    if (isPrime(num))
+	    {
+	      firstTenPrime = num;
+	      break;
+	    }
+	  }
+	
+	  System.out.println(Double.toString(firstTenPrime).substring(0, 1) + Double.toString(firstTenPrime).substring(2, 11));
 	}
 	
-
-	//Finish this function
-	public boolean isPrime(double d){
+	public boolean isPrime(double dNum)
+	{
+	  if (dNum < 2)
+	  {
+	    return false;
+	  }
+	  for (int i = 2; i < Math.sqrt(dNum); i++)
+	  {
+	    if (dNum % i == 0)
+	    {
+	      return false;
+	    }
+	  }
+	  return true;
 	}
-
+	
 }
 
